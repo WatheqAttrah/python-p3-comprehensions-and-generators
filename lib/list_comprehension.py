342ef2d0cd4487c0cefc21c4f9d4cd46f001f92d
Watheq Attrah <novaatrah@gmail.com>
2023-07-25 20:56:03 -0700

    Complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [number for number in num_list if number % 2 == 0]
+    return new_list
+        
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentence_list = [sentence + "!" for sentence in sentence_list]
+    return new_sentence_list
